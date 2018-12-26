ae99dbd1c937a20a5fa7191f736e90251dc71c6e
Adiwena Putra <adiwena0putra@gmail.com>
2018-12-26 10:48:56 +0700

    lanjut bikin API, bingung di read byte nya

--- GUI/SerialHandler-API/api_serialhandler.cpp
+++ GUI/SerialHandler-API/api_serialhandler.cpp
@@ -6,4 +6,40 @@ API_SerialHandler::API_SerialHandler(QObject *parent) : QObject(parent)
     m_infos = QSerialPortInfo::availablePorts();
 }
 
+void API_SerialHandler::open_serial()
+{
+    if (!m_isActive) {
+        m_serial.open(QIODevice::ReadWrite);
+        m_message = "SUCCED: Koneksi serial baru berhasil dibuat.";
+        emit message_SerialHandler(m_message);
+    } else {
+        m_message = "FAILED: Putuskan koneksi serial terlebih dahulu sebelum membuat koneksi serial baru.";
+        emit message_SerialHandler(m_message);
+    }
+}
+
+void API_SerialHandler::close_serial()
+{
+    if (m_isActive) {
+        m_serial.close();
+        m_message = "SUCCED: Koneksi serial berhasil diputuskan.";
+        emit message_SerialHandler(m_message);
+    } else {
+        m_message = "FAILED: Tidak ada koneksi serial untuk diputuskan.";
+        emit message_SerialHandler(m_message);
+    }
+}
+
+void API_SerialHandler::scan_serial()
+{
+    if (!m_isActive) {
+        m_infos = QSerialPortInfo::availablePorts();
+        m_message = "SUCCED: Scanning serial port selesai.";
+        emit message_SerialHandler(m_message);
+    } else {
+        m_message = "FAILED: Scanning bisa dimulai ketika semua koneksi serikal dimatikan.";
+        emit message_SerialHandler(m_message);
+    }
+}
+
 
--- GUI/SerialHandler-API/api_serialhandler.h
+++ GUI/SerialHandler-API/api_serialhandler.h
@@ -14,12 +14,22 @@ public:
     Q_INVOKABLE void open_serial();
     Q_INVOKABLE void close_serial();
     Q_INVOKABLE void scan_serial();
+    Q_INVOKABLE void set_serial(QSerialPort selected);
+    Q_INVOKABLE void set_baud(qint32 baud);
+    Q_INVOKABLE void set_databits(int databits);
+    Q_INVOKABLE void set_parity(int parity);
+    Q_INVOKABLE void set_stopbits(int stopbits);
 
 signals:
+    void message_SerialHandler(QString message);
+    void send_DataByte(QByteArray data);
 
 public slots:
+    void read_DataBytes();
+    void write_DataBytes(QByteArray data);
 
 private:
+    QString m_message;
     bool m_isActive;
     QSerialPort m_serial;
     QList<QSerialPortInfo> m_infos;
