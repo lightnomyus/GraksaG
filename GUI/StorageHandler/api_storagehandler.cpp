9a085df1d6d34300de29d9cccfe5b6574f0e0be7
Adiwena Putra <adiwena0putra@gmail.com>
2019-01-10 15:08:46 +0700

    lagi bikin fungsi write, masih progress

--- GUI/StorageHandler/api_storagehandler.cpp
+++ GUI/StorageHandler/api_storagehandler.cpp
@@ -2,10 +2,38 @@
 
 API_StorageHandler::API_StorageHandler(QObject *parent) : QObject(parent)
 {
-
+    m_isRead = false;
+    m_isWrite = false;
 }
 
 void API_StorageHandler::update_MissionName(QString missionName)
 {
     m_missionName = missionName;
+}
+
+void API_StorageHandler::start_Read()
+{
+    m_isRead = true;
+    m_isWrite = false;
+    m_file.setFileName(m_missionName);
+    m_file.open(QIODevice::ReadOnly);
+}
+
+void API_StorageHandler::start_Write()
+{
+    m_isWrite = true;
+    m_isRead = false;
+    m_file.setFileName(m_missionName);
+    m_file.open(QIODevice::WriteOnly);
+}
+
+void API_StorageHandler::write_Data(float alt, double lon, double lat,
+                                    float roll, float pitch, float yaw,
+                                    float xpos, float ypos, float zpos)
+{
+    if (m_isWrite) {
+        m_stream << alt << "," << lon << "," << lat << ","
+                 << roll << "," << pitch << "," << yaw << ","
+                 << xpos << "," << ypos << "," << zpos << "\n";
+    }
 }
--- GUI/StorageHandler/api_storagehandler.h
+++ GUI/StorageHandler/api_storagehandler.h
@@ -13,35 +13,26 @@ public:
     explicit API_StorageHandler(QObject *parent = nullptr);
 
 signals:
-    void update_UI_MissionLog(QString message);
-    void update_UI_Altitude(QString message);
-    void update_UI_RelativeRange(QString message);
-    void update_UI_MapPosition(QString message);
-    void update_UI_Gyro(QString message);
-    void update_UI_Accel(QString message);
-    void update_UI_Attitude(QString message);
+    void send_Attitude(float roll, float pitch, float yaw);
+    void send_Altitude(float alt);
+    void send_Coordinate(double lon, double lat);
+    void send_Odometry(float xpos, float ypos, float zpos);
 
 public slots:
+    void start_Write();
+    void start_Read();
     void update_MissionName(QString missionName);
-    void write_MissionLog(QString message);
-    void write_Altitude(float alt);
-    void write_RelativeRange(float range);
-    void write_MapPosition(double langitude, double latitude);
-    void write_Gyro(float gx, float gy, float gz);
-    void write_Accel(float ax, float ay, float az);
-    void write_Attitude(float roll, float pitch, float yaw);
-    void read_MissionLog();
-    void read_Altitude();
-    void read_RelativeRange();
-    void read_MapPosition();
-    void read_Gyro();
-    void read_Accel();
-    void read_Attitude();
+    void write_Data(float alt, double lon, double lat,
+                    float roll, float pitch, float yaw,
+                    float xpos, float ypos, float zpos);
+    void read_Data();
 
 private:
     QString m_missionName;
+    bool m_isRead;
     bool m_isWrite;
     QFile m_file;
+    QTextStream m_stream;
 };
 
 #endif // API_STORAGEHANDLER_H
